502f8460056392b41c438c6be49937ee8dca06ae
CristiFiodorov <cristifv59@gmail.com>
2023-11-01 01:03:57 +0200

    added lab3

---
+++ lab3/Collect/Collect/Collect.cpp
@@ -0,0 +1,131 @@
+﻿#include <Windows.h>
+#include <tchar.h>
+#include "defs.h"
+#include <string.h>
+#include <tlhelp32.h>
+
+
+DWORD writeProcessesToFile(LPCSTR fileName) {
+	HANDLE hProcessSnap;
+
+	hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	CHECK(hProcessSnap != INVALID_HANDLE_VALUE, 0, "Error at CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)");
+
+
+	PROCESSENTRY32 pe32;
+	pe32.dwSize = sizeof(pe32);
+	CHECK(Process32First(hProcessSnap, &pe32), 0, "Error at Process32First", CloseHandle(hProcessSnap));
+	
+	HANDLE hFile = CreateFile(fileName, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	CHECK(hFile != INVALID_HANDLE_VALUE, 0, "An error has occurred at CreateFile in writeProcessesToFile\n", CloseHandle(hProcessSnap));
+
+	DWORD count = 0;
+	do
+	{
+		char buffer[1000];
+		sprintf_s(buffer, "ParentProcessId: %d; ProcessId: %d; SzExeFile: %s\n", pe32.th32ParentProcessID ,pe32.th32ProcessID, pe32.szExeFile);
+		CHECK(WriteFile(hFile, buffer, strlen(buffer), NULL, NULL), 0, "Error at WriteFile", CloseHandle(hFile), CloseHandle(hProcessSnap));
+		++count;
+	} while (Process32Next(hProcessSnap, &pe32));
+
+	CloseHandle(hFile);
+	CloseHandle(hProcessSnap);
+	return count;
+}
+
+
+DWORD writeThreadsToFile(LPCSTR fileName) {
+	HANDLE hThreadSnap;
+
+	hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+	CHECK(hThreadSnap != INVALID_HANDLE_VALUE, 0, "Error at CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0)");
+
+
+	THREADENTRY32 te32;
+	te32.dwSize = sizeof(te32);
+	CHECK(Thread32First(hThreadSnap, &te32), 0, "Error at Thread32First", CloseHandle(hThreadSnap));
+
+	HANDLE hFile = CreateFile(fileName, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	CHECK(hFile != INVALID_HANDLE_VALUE, 0, "An error has occurred at CreateFile in writeThreadsToFile\n", CloseHandle(hThreadSnap));
+
+	DWORD count = 0;
+	do
+	{
+		char buffer[1000];
+		sprintf_s(buffer, "ThreadId: %d; OwnerProcessId: %d\n", te32.th32ThreadID, te32.th32OwnerProcessID);
+		CHECK(WriteFile(hFile, buffer, strlen(buffer), NULL, NULL), 0, "Error at WriteFile", CloseHandle(hFile), CloseHandle(hThreadSnap));
+		++count;
+	} while (Thread32Next(hThreadSnap, &te32));
+
+	CloseHandle(hFile);
+	CloseHandle(hThreadSnap);
+	return count;
+}
+
+
+BOOL writeModulesToFile(LPCSTR fileName) {
+	HANDLE hModuleSnap;
+
+	hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, GetCurrentProcessId());
+	CHECK(hModuleSnap != INVALID_HANDLE_VALUE, 0, "Error at CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, 0)");
+
+
+	MODULEENTRY32 me32;
+	me32.dwSize = sizeof(me32);
+	CHECK(Module32First(hModuleSnap, &me32), 0, "Error at Module32First", CloseHandle(hModuleSnap));
+
+	HANDLE hFile = CreateFile(fileName, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	CHECK(hFile != INVALID_HANDLE_VALUE, 0, "An error has occurred at CreateFile in writeModulesToFile\n", CloseHandle(hModuleSnap));
+
+	DWORD count = 0;
+	do
+	{
+		char buffer[1000];
+		sprintf_s(buffer, "ModuleID: %d; ProcessId: %d; szModule: %s; szExePath: %s\n", 
+			me32.th32ModuleID, me32.th32ProcessID, me32.szModule, me32.szExePath);
+		CHECK(WriteFile(hFile, buffer, strlen(buffer), NULL, NULL), 0, "Error at WriteFile", CloseHandle(hFile), CloseHandle(hModuleSnap));
+		++count;
+	} while (Module32Next(hModuleSnap, &me32));
+
+	CloseHandle(hFile);
+	CloseHandle(hModuleSnap);
+	return count;
+}
+
+
+int main()
+{
+	LPCSTR parts[] = { "Week3", "ProcessInfo" };
+	char dirPath[MAX_PATH] = "C:\\Facultate\\CSSO\\Laboratoare";
+
+	for (auto part : parts) {
+		sprintf_s(dirPath, "%s\\%s", dirPath, part);
+		BOOL dirReturn = CreateDirectory(dirPath, NULL);
+		CHECK(dirReturn != 0 || GetLastError() == ERROR_ALREADY_EXISTS, 1, "An error has occurred at creating a directory!\n");
+	}
+
+	char fileName[MAX_PATH];
+
+	sprintf_s(fileName, "%s\\%s", dirPath, "procese.txt");
+	DWORD processes = writeProcessesToFile(fileName);
+
+	sprintf_s(fileName, "%s\\%s", dirPath, "fire.txt");
+	DWORD threads = writeThreadsToFile(fileName);
+
+	sprintf_s(fileName, "%s\\%s", dirPath, "module_process.txt");
+	DWORD modules = writeModulesToFile(fileName);
+
+
+	char buffer[1000];
+	sprintf_s(buffer, "1. \"Module: %d\" \n2. \"Procese: %d\" \n3. \"Fire : %d\"\n", modules, processes, threads);
+
+	HANDLE hMapping = OpenFileMappingA(FILE_MAP_READ | FILE_MAP_WRITE, FALSE, "cssoh3basicsync");
+	CHECK(hMapping != NULL, -1, "Error at OpenFileMappingA");
+
+	void* fileMapP = MapViewOfFile(hMapping, FILE_MAP_WRITE, 0, 0, 0);
+	CHECK(fileMapP != NULL, -1, "Error at MapViewOfFile", CloseHandle(hMapping));
+	memcpy(hMapping, buffer, strlen(buffer));
+
+	CloseHandle(hMapping);
+	return 0;
+}
---
+++ lab3/Filter/Filter/Filter.cpp
@@ -0,0 +1,102 @@
+#include <Windows.h>
+#include <tchar.h>
+#include "defs.h"
+
+
+BOOL setPrivilege(LPCSTR privilegeName) {
+	LUID luid;
+	CHECK(LookupPrivilegeValue(NULL, privilegeName, &luid), FALSE, "Error at LookupPrivilegeValue\n");
+
+	HANDLE hToken;
+	CHECK(OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken), FALSE, "Error at OpenProcessToken\n");
+
+	TOKEN_PRIVILEGES tokenPrivileges;
+	tokenPrivileges.PrivilegeCount = 1;
+	tokenPrivileges.Privileges[0].Luid = luid;
+	tokenPrivileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	
+	BOOL res = AdjustTokenPrivileges(hToken, FALSE, &tokenPrivileges, sizeof(TOKEN_PRIVILEGES), NULL, NULL);
+	CHECK(res, FALSE, "Error at AdjustTokenPrivileges\n", CloseHandle(hToken));
+	
+	CloseHandle(hToken);
+	return TRUE;
+}
+
+DWORD WINAPI searchInFile(LPVOID lpThreadParameter)
+{
+	char* fileName = *(char**)lpThreadParameter;
+	char* searchString = ((char**)lpThreadParameter)[1];
+	
+	HANDLE hFile = CreateFile(fileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	CHECK(hFile != INVALID_HANDLE_VALUE, -1, "An error has occurred at CreateFile in searchInFile\n");
+
+	DWORD fileSize = GetFileSize(hFile, NULL);
+	CHECK(fileSize != INVALID_FILE_SIZE, -1, "An error has occurred at GetFileSize in searchInFile\n", CloseHandle(hFile));
+	
+	char* fileContent = new char[fileSize + 1];
+	memset(fileContent, 0, fileSize + 1);
+	CHECK(ReadFile(hFile, fileContent, fileSize, NULL, NULL), -1, "An error has occurred at ReadFile in searchInFile\n", CloseHandle(hFile));
+
+	DWORD count = 0;
+	char* p = fileContent;
+	
+	while (*p != '\0') {
+		while (*p != '\n' && *p != '\0') {
+			p++;
+		}
+		if (*p == '\n') {
+			*p = '\0';
+			
+			for (int i = 0; i < strlen(fileContent) - strlen(searchString) + 1; i++) {
+				if (searchString[0] == fileContent[i]) {
+					for (int j = 1; j < strlen(searchString); j++) {
+						if (searchString[j] != fileContent[i + j]) {
+							break;
+						}
+						if (j == strlen(searchString) - 1) {
+							count++;
+						}
+					}
+				}
+			}
+			p++;
+			fileContent = p;
+		}
+	}
+
+	printf("%s: %d\n", fileName, count);
+	return count;
+}
+
+
+int main() {
+	setPrivilege(SE_SYSTEM_PROFILE_NAME);
+
+	LPCSTR fileNames[3] = {
+		"C:\\Facultate\\CSSO\\Laboratoare\\Week3\\ProcessInfo\\fire.txt",
+		"C:\\Facultate\\CSSO\\Laboratoare\\Week3\\ProcessInfo\\module_process.txt",
+		"C:\\Facultate\\CSSO\\Laboratoare\\Week3\\ProcessInfo\\procese.txt"
+	};
+
+	HANDLE hThreads[3];
+	LPCSTR* params[3];
+
+	for (int i = 0; i < 3; i++) {
+		params[i] = new LPCSTR[2]{ fileNames[i], "Discord.exe" };
+		hThreads[i] = CreateThread(NULL, 0, searchInFile, params[i], 0, NULL);
+		CHECK(hThreads[i] != NULL, -1, "Error at CreateThread\n");
+	}
+
+	WaitForMultipleObjects(3, hThreads, TRUE, INFINITE);
+
+
+	for (int i = 0; i < 3; i++) {
+		CloseHandle(hThreads[i]);
+		delete[] params[i];
+	}
+	
+	printf("Sleep\n");
+	Sleep(1000 * 1800);
+	
+	return 0;
+}
